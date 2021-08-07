8e2b89f30afc94df2974d26fec6e3435a03843e2
Unoxxis <boris@yfran.samirien>
2021-08-07 20:25:00 +0200

    Change venv version checking to submodule.

--- setup-venv.sh
+++ setup-venv.sh
@@ -12,7 +12,7 @@ echo "Checking for pyenv..."
 if ! command -v pyenv 1>/dev/null 2>&1; then
 	echo 'pyenv and pyenv-virtualenv are required for this script!'
 	echo 'Please see https://github.com/pyenv/pyenv and https://github.com/pyenv/pyenv-virtualenv'
-	exit 1
+	exit 99
 fi
 echo "Activating pyenv..."
 eval "$(pyenv init -)"
@@ -23,11 +23,11 @@ eval "$(pyenv virtualenv-init -)"
 #----------------
 
 # Install the Python version, if not already installed
-if pyenv versions --bare | grep -q $PY_VERSION; then
+if pyenv versions --bare | grep -q "$PY_VERSION"; then
 	echo "Python Version <$PY_VERSION> already present."
 else
 	echo "Installing Python version <$PY_VERSION>..."
-	pyenv install --skip-existing $PY_VERSION
+	pyenv install --skip-existing "$PY_VERSION"
 fi
 
 #-------------------------
@@ -36,15 +36,36 @@ fi
 echo ""
 echo "*** SETUP DEVELOPMENT ENVIRONMENT ***"
 
-# Check if Virtual environment for development is already there
-if pyenv virtualenvs --bare | grep -q $DEV_ENV_NAME; then
-	echo "Virtual environment <$DEV_ENV_NAME> already present."
-else
-	echo "Creating virtual environment <$DEV_ENV_NAME>..."
-	pyenv virtualenv $PY_VERSION $DEV_ENV_NAME
+echo "Checking if virtual environment $DEV_ENV_NAME already exists..."
+./venv-tools/venv-check-version "$DEV_ENV_NAME" "$PY_VERSION" 1>/dev/null 2>&1
+case $? in
+	0)	echo "Virtual environment exists and has correct python version."
+		;;
+	1)  echo "Virtual environment exists but is for a different python version $(./venv-tools/venv-check-version "$DEV_ENV_NAME")!"
+		# TODO Add an option to save and restore old version requirement data.
+		read -p "Reinstall with correct python version $PY_VERSION? " -n 1 -r
+		echo
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo "Deleting old virtual environment..."
+			pyenv virtualenv-delete "$DEV_ENV_NAME"
+			createenv='yes'
+		else
+			echo "The script will still try to install correct dependencies."
+			echo "However, it is not guaranteed that it will work if the versions"
+			echo "differ by substantial amount."
+		fi
+		;;
+	2)	echo "Virtual environment does not exist and will be created."
+		createenv='yes'
+esac
+
+if [[ "$createenv" == "yes" ]]; then
+	echo "Creating virtual environment <$DEV_ENV_NAME> ..."
+	pyenv virtualenv "$PY_VERSION" "$DEV_ENV_NAME"
+	createenv=
 fi
 
-# TODO Check if virtual environment is correct python version!
+
 
 echo "Setting local virtual environment for pyenv..."
 pyenv local $DEV_ENV_NAME
